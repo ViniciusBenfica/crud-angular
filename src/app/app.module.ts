26aece92d41500b927c97b285b1fe0ecca5bbfc1
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-04-20 00:27:03 -0300

    create project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstPageComponent } from './components/first-page/first-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstPageComponent,
+    LoginComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
