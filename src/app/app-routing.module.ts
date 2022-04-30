5afead7d81710aed40f869d7b923bb7d439cbea8
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-04-30 15:27:26 -0300

    create component button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstPageComponent } from './components/first-page/first-page.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path: '', component: FirstPageComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'sign-up', component: SignUpComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FirstPageComponent } from './components/first-page/first-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstPageComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
